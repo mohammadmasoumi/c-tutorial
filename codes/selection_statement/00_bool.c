7be2e2441c74704c75ce7637c0a9696a6934bc0a
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-08-04 01:05:31 +0430

    update files

--- codes/selection_statement/00_bool.c
+++ codes/selection_statement/00_bool.c
@@ -7,12 +7,14 @@ int main() {
     // https://stackoverflow.com/questions/1608318/is-bool-a-native-c-type
     // bool type in C89/90
     // bool keyword is native in C99
-    _Bool boolean;
+    _Bool C89C90boolean = true;
+    bool C99Boolean = true;
 
-    boolean = true;
+    if (C89C90boolean)
+        printf("Boolean in C89/C90.\n");
 
-    if (boolean)
-        printf("Helllo this is a boolean");
+    if (C99Boolean) 
+        printf("Boolean in C99.\n");
 
     return 0;
 }
--- codes/selection_statement/01_ternary_operator.c
+++ codes/selection_statement/01_ternary_operator.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
-
 
 int main() {
 
-
-    _Bool bool1;
-
-    bool1 = true;
-
-
-    if (bool1)
-        printf("Helllo this is bool1");
-
-
+    int grade = 60;
+    printf(grade >= 60 ? "Passed\n" : "Failed\n");
 
     return 0;
 }
