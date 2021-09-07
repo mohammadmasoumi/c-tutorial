9ec19d9b121e32586471c3dcd2216a6852efe02d
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-07 22:42:39 +0430

    update MemoryManagement and pointer section

---
+++ tutorial/16_Pointers/01_basic_concept.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+
+    /*
+    Pointer: pointer is a varible whose address is the address of another variable.
+
+
+    */
+
+    int aVariable = 10;
+
+    printf("-------------------------------------------");
+    printf("The value of aVariable: {%d}", aVariable);
+    printf("The address of aVariable: {%d}", aVariable);
+    printf("-------------------------------------------");
+
+    return 0;
+}
