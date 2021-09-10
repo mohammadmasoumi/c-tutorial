9a8e3dee3b3597bc341e2da3416fa13cac07a79e
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-11 00:56:25 +0430

    add list of string

---
+++ tutorial/15_Arrays/05_arrays_of_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+const int WORD_COUNT = 5;
+const int MAX_WORD_LIST = 256;
+
+
+int main() {
+    char myList[WORD_COUNT][MAX_WORD_LIST];
+
+    for (int i = 0; i < WORD_COUNT; i++) {
+        scanf("%s", myList[i]);
+    }
+
+    printf("------------- Names: -------------\n");
+    for (int i = 0; i < WORD_COUNT; i++) {
+        printf("%s\n", myList[i]);
+    }
+
+    return 0;
+}
