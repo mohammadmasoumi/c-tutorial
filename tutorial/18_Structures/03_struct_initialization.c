a6afe2b4b429b66fae51bc197b0518812df389ad
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-12 02:25:26 +0430

    update struct_initialization

--- tutorial/18_Structures/03_struct_initialization.c
+++ tutorial/18_Structures/03_struct_initialization.c
@@ -26,6 +26,10 @@ int main() {
     p3.x = 0;
     p3.y = 1;
 
+    printf("p1.x: %d, p1.y: %d\n", p1.x, p1.y);
+    printf("p2.x: %d, p2.y: %d\n", p2.x, p2.y);
+    printf("p3.x: %d, p3.y: %d\n", p3.x, p3.y);
+
 
     return 0;
 }
