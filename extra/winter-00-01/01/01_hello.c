2441f305740c15dcb1b35af0b109f10cc3f0d3a2
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-18 15:05:13 +0330

    add first session

---
+++ extra/winter-00-01/01/01_hello.c
@@ -0,0 +1,18 @@
+// comment
+
+#include <stdio.h>
+
+int main() {
+
+    // define a varible named `number`
+    // each variable has:
+    //  1. name: number
+    //  2. type: int
+    //  3. value: 13
+    int number = 13;
+
+    // print the value of number
+    printf("%d", number); // DO NOT FORGET ;
+
+    return 0; 
+}
